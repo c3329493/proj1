4d7b8f395da13b613ea7712f44b54f7768012ab7
Alexander  Stamenkovski <c3329493@uon.edu.au>
2019-04-04 05:34:24 +0000

    modulus

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+void printsomething ();
+int x = 20;
 int main ()
 {
-    intx = 5
-    
-return 0;
+printf("I have %d watermelons\n", x);
 }
+void printsomething () {
+   printf("I have %d watermelons\n", x);
+    return;
+} 
+
+ 
+65>letter>90
+(L+K)%26
